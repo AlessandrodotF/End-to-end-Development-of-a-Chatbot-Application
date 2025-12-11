286d43e66f07837c4e34d4e2fe152b7d994f8ef9
AlessandrodotF <alessandrofella@live.it>
2025-12-11 10:12:20 +0100

    some changes

--- ChatBot/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-AWS_REGION = os.getenv("AWS_REGION",None)
-AWS_PROFILE = os.getenv("AWS_PROFILE",None)
-MODEL_NAME = "us.anthropic.claude-haiku-4-5-20251001-v1:0"
-
-TEMPERATURE = 0.0
-MAX_TOKENS =1024
